f6807092b42af899b6d8a6891be551cf18ad843e
Elijah Zimmerman <elijahz0409@gmail.com>
2024-11-24 14:28:03 -0500

    Added ffmpeg install check

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from controller import Controller
 from model import Model
 from view import View
 
+from shutil import which
+from tkinter import messagebox
+
 if __name__ == "__main__":
+    if which("ffmpeg") is None:
+        messagebox.showerror("Error", "Could not locate ffmpeg.")
+        quit(1)
     model = Model()
     view = View()
     controller = Controller(model, view)
