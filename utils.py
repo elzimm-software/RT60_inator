a919d580fb1d8368cff611338f34a36dd73854b0
Elijah Zimmerman <elijahz0409@gmail.com>
2024-11-26 18:23:24 -0500

    Implemented low and high pass filters

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 import numpy as np
+from scipy.signal import butter, filtfilt
 
+def low_pass(signal, cut, fs):
+    nyq = 0.5 * fs
+    normal_cut = cut / nyq
+    b, a = butter(4, normal_cut, btype='lowpass')
+    return filtfilt(b, a, signal)
+
+def high_pass(signal, cut, fs):
+    nyq = 0.5 * fs
+    normal_cut = cut / nyq
+    b, a = butter(4, normal_cut, btype='highpass')
+    return filtfilt(b, a, signal)
 
 def digital_to_decibel(signal: int):
     if signal > 0:
